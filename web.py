a4c49a75be1ec14c95ab31ae9ab178bc47ae2d03
Michaël Dierick <michael@dierick.io>
2016-10-29 17:19:22 +0200

    Fix process args (set empty)

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ from .scraper import scrape
 @app.route("/")
 def exampleMethod():
     # Make the scraping asynchronous for not timing out the http-request
-    p = Process(target=scrape) #args=('bob',)
+    p = Process(target=scrape, args=()) #args=('bob',)
     p.start()
     p.join(5) #Wait at most x seconds for the scraper to return, (blocking for x seconds)
     if p.exitcode == 0:
